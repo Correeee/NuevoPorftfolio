2c6fe93bf2d64acc296ac532c6e2c776288dbc9f
elcorre <correa755@hotmail.com>
2023-04-09 15:28:42 -0300

    Responsive

--- js/script.js
+++ js/script.js
@@ -2,6 +2,12 @@ let progressNumber = document.getElementById('progressNumber')
 let count = 0
 const finalCount = 50
 
+const html = document.getElementById('html')
+const css = document.getElementById('css')
+const javascript = document.getElementById('javascript')
+const bootstrap = document.getElementById('bootstrap')
+const react = document.getElementById('react')
+
 setTimeout(() => {
     const interval = setInterval(() => {
         count += 1
@@ -9,15 +15,21 @@ setTimeout(() => {
         if(count == finalCount){
             clearInterval(interval)
         }
-        // if(count == 10){
-        //     console.log('Desarrollo Web')
-        // }
-        // if(count == 25){
-        //     console.log('Javacript')
-        // }
-        // if(count == 50){
-        //     console.log('React JS')
-        // }
+        if(count == 10){
+            html.style.display = 'flex'
+        }
+        if(count == 15){
+            css.style.display = 'flex'
+        }
+        if(count == 20){
+            bootstrap.style.display = 'flex'
+        }
+        if(count == 30){
+            javascript.style.display = 'flex'
+        }
+        if(count == 45){
+            react.style.display = 'flex'
+        }
 
     }, 60);
     
